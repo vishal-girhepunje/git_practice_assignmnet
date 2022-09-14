6570854270c2c3291c43ed70e8affc788edf9d5d
Shahid77137 <siddiquibabuddin202@gmail.com>
2022-09-14 09:26:53 +0530

    minor changes in pallindrome and prime

--- problem1_pallindrom.js
+++ problem1_pallindrom.js
@@ -4,9 +4,9 @@
  while(num>0){
     rem=num%10;
     num=Math.floor(num/10);
-    rev=rev*10;
+    rev=rev*10+rem;
     }
-    if(num==rev){
+    if(temp==rev){
         console.log("Yes");
     }else{
         console.log("No");
